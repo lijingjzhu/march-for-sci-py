98637c61cfc8b568629639a7985f05f0982111e9
Julie Zhu <lijingjzhu@users.noreply.github.com>
2019-05-05 12:30:52 -0700

    Update guessing_game with enhancements

--- guessing_game.py
+++ guessing_game.py
@@ -1,19 +1,23 @@
 # Package for random number selection
 import random 
 
+# Parameters
+min_number = 1
+max_number = 10
+
 # Pick the number to guess
-n = random.randint(1, 99)
+n = random.randint(min_number, max_number)
 
 # Instructions for the guessing game
 print ("Try to guess the number I am thinking about...")
-guess = int(input("Enter an integer from 1 to 99: "))
+guess = int(input("Enter an integer from " + str(min_number) + " to "+ str(max_number) + ": "))
 
 while n != guess:
   if guess < n:
     print ("Looks like that number is low...")
-    guess = int(input("Enter an integer from 1 to 99: "))
+    guess = int(input("Enter an integer from " + str(min_number) + " to "+ str(max_number) + ": "))
   elif guess > n:
     print ("Too high! Chill the number down!")
-    guess = int(input("Enter an integer from 1 to 99: "))
+    guess = int(input("Enter an integer from " + str(min_number) + " to "+ str(max_number) + ": "))
 # Once the number is the same, we can quit out of the game
 print ("Spot on! That was an optimal guess.")
